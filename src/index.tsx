e68105b409bf5ac5b977912f71e9c85f3083d2cb
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-05-02 14:57:57 +0900

    feat: add simple Dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import Dashboard from "./pages/Dashboard"
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById("root")
 )
