fe31d8f706333324cb684eab175aad71c7942388
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-05-05 21:34:44 +0900

    fix: fix warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 
 import Dashboard from "./pages/Dashboard"
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Dashboard />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const container = document.getElementById("root")
+
+if (container) {
+  const root = createRoot(container) 
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>
+    </React.StrictMode>
+  )
+}
