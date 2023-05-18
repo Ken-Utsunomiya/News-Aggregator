5e047e01aeac03c7bdf106903697cf2450f7d654
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-05-18 11:19:35 +0900

    fix: rename file name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import HeadlinesContainer from "../components/headlines/HeadlinesContainer"
+import HeadlinesContainer from "../components/headlines/HeadlineListContainer"
 
 
 const Home = () => {
