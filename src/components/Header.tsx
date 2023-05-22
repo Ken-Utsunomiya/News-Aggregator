294d218925c358aa5dba2072ba6d346e46e8d234
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-05-22 12:05:54 +0900

    feat: add header stub

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
 import React from "react"
 
+import { useAppSelector } from "../redux/hooks"
+import { selectLanguage, setLanguage } from "../redux/languages/languageSlice"
+
 const Header = () => {
+  const selectedLanguage = useAppSelector(selectLanguage)
+
+  const handleLanguageChange = (language: string) => {
+    setLanguage(language)
+  }
+
   return (
-    <div>
-      Header
+    <div  className="flex items-center justify-between">
+      <div>
+        Header
+      </div>
+      <div>
+        <button
+          onClick={ () => handleLanguageChange("en") }
+          disabled={ selectedLanguage === "en" }
+        >
+          EN
+        </button>
+        <button
+          onClick={ () => handleLanguageChange("jp") }
+          disabled={ selectedLanguage === "jp" }
+        >
+          JP
+        </button>
+      </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import Header from "../components/Header"
 import HeadlinesContainer from "../components/headlines/HeadlineListContainer"
 
 import "../styles/index.css"
@@ -8,6 +9,7 @@ import "../styles/index.css"
 const Home = () => {
   return (
     <div>
+      <Header />
       <HeadlinesContainer />
     </div>
   )
