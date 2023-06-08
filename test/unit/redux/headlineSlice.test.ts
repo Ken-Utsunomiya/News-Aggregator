09e7bf2211c1948c58a1e1af34778f1f8d7e80b2
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-06-08 13:38:38 +0900

    add passing test for headlineSlice

---
+++ test/unit/redux/headlineSlice.test.ts
@@ -0,0 +1,52 @@
+import configureStore from "redux-mock-store"
+import thunk from "redux-thunk"
+
+import { fetchHeadline, initialState } from "../../../src/redux/articles/headlinesSlice"
+import * as NewsAPIClient from "../../../src/services/NewsAPIClient"
+
+import type { HeadlinesState } from "../../../src/types/articles"
+import type { AnyAction } from "redux"
+import type { MockStoreEnhanced } from "redux-mock-store"
+import type { ThunkDispatch } from "redux-thunk"
+
+interface AppState {
+  headlines: HeadlinesState
+}
+type AppDispatch = ThunkDispatch<AppState, void, AnyAction>
+
+const mockStore = configureStore<AppState, AppDispatch>([thunk])
+
+jest.mock("../../../src/services/NewsAPIClient")
+
+describe("async thunks", () => {
+  let store: MockStoreEnhanced<AppState, AppDispatch>
+
+  beforeEach(() => {
+    store = mockStore({ headlines: initialState })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test("fetchHeadline should dispatch the correct actions", async () => {
+    const expectedCategory = "technology"
+    const expectedCountry = "us"
+    const mockResponse = {
+      category: expectedCategory,
+      headline: [{ title: "Headline 1" }, { title: "Headline 2" }],
+    }
+
+    jest.spyOn(NewsAPIClient, "getHeadline").mockResolvedValueOnce(mockResponse)
+
+    await store.dispatch(fetchHeadline({
+      category: expectedCategory, 
+      country: expectedCountry
+    }))
+
+    const actions = store.getActions()
+    expect(actions).toHaveLength(2)
+    expect(actions[0]).toMatchObject({ type: fetchHeadline.pending.type })
+    expect(actions[1]).toMatchObject({ type: fetchHeadline.fulfilled.type })
+  })
+})
