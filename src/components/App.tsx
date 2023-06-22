1d9570ba321da172c3a7c6709908741005ea45bc
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-06-22 13:28:02 +0900

    feat: introduce theme and styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,28 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+import { ThemeProvider as MaterialThemeProvider } from "@mui/material/styles"
+import { ThemeProvider as StyledThemeProvider } from "styled-components"
+
 import Dashboard from "../pages/Dashboard"
+import theme from "../styles/theme"
 
 import Header from "./Header"
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Dashboard />} />
-          <Route path="/headlines" element={<div />} />
-        </Routes>
-      </BrowserRouter>
+      <MaterialThemeProvider theme={theme}>
+        <StyledThemeProvider theme={theme}>
+          <Header />
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Dashboard />} />
+              <Route path="/headlines" element={<div />} />
+            </Routes>
+          </BrowserRouter>
+        </StyledThemeProvider>
+      </MaterialThemeProvider>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,49 +46,52 @@ import IconButton from "@mui/material/IconButton"
 import InputBase from "@mui/material/InputBase"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
-import { styled, alpha } from "@mui/material/styles"
+import { alpha } from "@mui/material/styles"
+// eslint-disable-next-line import/no-named-as-default
+import styled from "styled-components"
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}))
+const Search = styled.div`
+  position: relative;
+  border-radius: ${({ theme }) => theme.shape.borderRadius};
+  background-color: ${({ theme }) =>
+    alpha(theme.palette.common.white, 0.15)};
+  &:hover {
+    background-color: ${({ theme }) => alpha(theme.palette.common.white, 0.25)};
+  }
+  margin-left: 0;
+  width: 100%;
+  ${({ theme }) => theme.breakpoints.up("sm")} {
+    margin-left: ${({ theme }) => theme.spacing(1)};
+    width: auto;
+  }
+`
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}))
+const SearchIconWrapper = styled.div`
+  padding: ${({ theme }) => theme.spacing(0, 2)};
+  height: 100%;
+  position: absolute;
+  pointer-events: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}))
+const StyledInputBase = styled(InputBase)`
+  color: inherit;
+  & .MuiInputBase-input {
+    padding: ${({ theme }) => theme.spacing(1, 1, 1, 0)};
+    padding-left: ${({ theme }) => `calc(1em + ${theme.spacing(4)})`};
+    transition: ${({ theme }) => theme.transitions.create("width")};
+    width: 100%;
+    ${({ theme }) => theme.breakpoints.up("sm")} {
+      width: 12ch;
+      &:focus {
+        width: 20ch;
+      }
+    }
+  }
+`
+
 
 export default function SearchAppBar() {
   return (
@@ -110,7 +113,7 @@ export default function SearchAppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            News
           </Typography>
           <Search>
             <SearchIconWrapper>
