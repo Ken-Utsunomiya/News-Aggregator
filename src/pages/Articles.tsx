f6896d56ea886ccf480f8ff12a0bbd0e7dd85fe9
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-06-29 12:29:21 +0900

    feat: call dispatch in Articles

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,6 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+import { fetchArticles } from "../redux/articles/articlesSlice"
+import { fetchHeadline } from "../redux/articles/headlinesSlice"
+import { selectCategory } from "../redux/category/categorySlice"
+import { useAppDispatch, useAppSelector } from "../redux/hooks"
+import { selectLanguage } from "../redux/languages/languageSlice"
 
 const Articles = () => {
+  const dispatch = useAppDispatch()
+  const selectedCategory = useAppSelector(selectCategory)
+  const selectedLanguage = useAppSelector(selectLanguage)
+
+  useEffect(() => {
+    if (selectedCategory) {
+      dispatch(
+        fetchHeadline({
+          category: selectedCategory,
+          country: selectedLanguage
+        })
+      )
+    } else {
+      dispatch(
+        fetchArticles({
+          language: selectedLanguage,
+          q: ""
+        })
+      )
+    }
+  })
+  
   return (
     <div></div>
   )
